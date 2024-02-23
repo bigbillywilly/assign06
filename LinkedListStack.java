9e484660d367df90f6a6eee8810e457b63099b6b
William Ngo <89047738+bigbillywilly@users.noreply.github.com>
2024-02-23 14:48:06 -0700

    Update LinkedListStack.java

--- LinkedListStack.java
+++ LinkedListStack.java
@@ -1,5 +1,76 @@
 package assign06;
 
-public class LinkedListStack {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+public class LinkedListStack<E> implements Stack<E> {
+
+	private SinglyLinkedList<E> list;
+	private int size;
+	
+	public LinkedListStack() {
+		this.list = new SinglyLinkedList<>();
+		this.size = list.size();
+	}
+
+	/**
+	 * Removes all of the elements from the stack.
+	 */
+	@Override
+	public void clear() {
+		list.clear();
+	}
+
+	/**
+	 * @return true if the stack contains no elements; false, otherwise.
+	 */
+	@Override
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	/**
+	 * Returns, but does not remove, the element at the top of the stack.
+	 * 
+	 * @return the element at the top of the stack
+	 * @throws NoSuchElementException if the stack is empty
+	 */
+	@Override
+	public E peek() throws NoSuchElementException{
+		if(this.isEmpty())
+			throw new NoSuchElementException("List is empty");
+		return list.getFirst();
+	}
+
+	/**
+	 * Returns and removes the item at the top of the stack.
+	 * 
+	 * @return the element at the top of the stack
+	 * @throws NoSuchElementException if the stack is empty
+	 */
+	@Override
+	public E pop() throws NoSuchElementException{
+		if(this.isEmpty())
+			throw new NoSuchElementException("List is empty");
+		return list.deleteFirst();
+	}
+
+	/**
+	 * Adds a given element to the stack, putting it at the top of the stack.
+	 * 
+	 * @param element - the element to be added
+	 */
+	@Override
+	public void push(E element) {
+		list.insertFirst(element);
+	}
+	
+	/**
+	 * @return the number of elements in the stack
+	 */
+	@Override
+	public int size() {
+		return size;
+		
+	}
 }
