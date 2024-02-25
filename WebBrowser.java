f1013009e487f3a25ebd41e818aa50318e37b2b9
Elijah <117758971+Celestron1123@users.noreply.github.com>
2024-02-24 20:01:47 -0700

    Update WebBrowser.java

--- WebBrowser.java
+++ WebBrowser.java
@@ -1,5 +1,61 @@
 package assign06;
 
-public class WebBrowser {
+import java.net.URL;
+import java.util.NoSuchElementException;
 
+public class WebBrowser {
+	private Stack<URL> forward = new LinkedListStack<>();
+	private Stack<URL> backward = new LinkedListStack<>();
+	private URL current;
+
+	public WebBrowser() {
+
+	}
+
+	public WebBrowser(SinglyLinkedList<URL> history) {
+		if (!history.isEmpty()) {
+			current = history.getFirst();
+			for (int i = history.size() - 1; i > 0; i--) {
+				backward.push(history.get(i));
+			}
+		}
+	}
+
+	public void visit(URL webpage) {
+		forward.clear();
+		backward.push(current);
+		current = webpage;
+	}
+
+	public URL back() throws NoSuchElementException {
+		if (backward.isEmpty())
+			throw new NoSuchElementException("There is no previously visited URL!");
+		forward.push(current);
+		current = backward.pop();
+		return current;
+	}
+
+	public URL forward() throws NoSuchElementException {
+		if (forward.isEmpty())
+			throw new NoSuchElementException("There is no URL to visit!");
+		backward.push(current);
+		current = forward.pop();
+		return current;
+	}
+
+	public SinglyLinkedList<URL> history() {
+		SinglyLinkedList<URL> hist = new SinglyLinkedList<>();
+		if (!backward.isEmpty()) {
+			int limit = backward.size();
+			for (int i = 0; i < limit; i++) {
+				hist.insert(i, backward.pop());
+			}
+			for (int i = 0; i < hist.size(); i++) {
+				backward.push(hist.get(i));
+			}
+			hist.insertFirst(current);
+			return hist;
+		} else
+			return hist;
+	}
 }
